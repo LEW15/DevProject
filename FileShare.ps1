795c7ff6dfc0f366f3a3223e8630e5bbb01c870a
LEW15 <115412568+LEW15@users.noreply.github.com>
2023-02-17 15:38:02 +0000

    Update FileShare.ps1

--- FileShare.ps1
+++ FileShare.ps1
@@ -1,8 +1,8 @@
 #Create a Fileshare
-New-SmbShare -Name "FileShare" -Path "D:\" -FullAccess Everyone
+New-SmbShare -Name "FileShare" -Path "D:\" -FullAccess Everyone -wait
 
 #Download the Sketchup installer and save to fileshare
-Invoke-WebRequest -Uri "https://www.sketchup.com/sketchup/SketchUpPro-full-exe" -OutFile "C:\FileShare\SketchUpPro2022Installer.exe"
+Invoke-WebRequest -Uri "https://www.sketchup.com/sketchup/SketchUpPro-full-exe" -OutFile "\\vmdesign1\FileShare\SketchUpPro2022Installer.exe"
 
 #Download PRTG installer and save to fileshare
-#Invoke-WebRequest -Uri "https://www.paessler.com/download/prtg-download?download=1" -OutFile "C:\FileShare\PRTG.exe"
+#Invoke-WebRequest -Uri "https://www.paessler.com/download/prtg-download?download=1" -OutFile "\\vmdesign1\FileShare\PRTG.exe"
